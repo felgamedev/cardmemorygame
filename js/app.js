0f371d8aa6cd22b9e6838318cbdb2a071288ec08
felgamedev <felgamedev@gmail.com>
2018-03-01 13:55:45 -0400

    Linting JS file for errors

--- js/app.js
+++ js/app.js
@@ -13,74 +13,79 @@ const timerSecs = document.createElement('span');
 
 let cards = [];
 const openCards = [];
-var timeoutId = null;
-var timerIntervalId = null;
+let timeoutId = null;
+let timerIntervalId = null;
 let movesCounter = 0;
 let matchingPairsCount = 0;
 let starsCount = 3;
 let starsElements = scorePanel.querySelectorAll('i');
 // Timer variables
-let startTime = nowTime = minutes = seconds = 0;
+let startTime = 0;
+let nowTime = 0;
+let minutes = 0;
+let seconds = 0;
 
 // Event Listeners
 deck.addEventListener('click', function(event){
-	let cardClicked;
-	// Check to see if a card has been clicked
-	if(event.target.nodeName == "LI"){ // Card area is clicked
-		cardClicked = event.target;
-	} else if(event.target.nodeName == "I"){ // Shown card icon is clicked
-		cardClicked = event.target.parentElement;
-	}else return; // Empty deck area clicked
+  let cardClicked;
+  // Check to see if a card has been clicked
+  if(event.target.nodeName == "LI"){ // Card area is clicked
+    cardClicked = event.target;
+  } else if(event.target.nodeName == "I"){ // Shown card icon is clicked
+    cardClicked = event.target.parentElement;
+  } else {
+    return; // Empty deck area clicked
+  }
 
-	// Valid card selected, start timer if not already running
-	if(startTime < 1) startTimer();
+  // Valid card selected, start timer if not already running
+  if(startTime < 1) {startTimer();}
 
-	// Don't do anything if the card has already been matched
-	if(cardClicked.classList.contains('match')) return;
-	// Prevent clicking the same card twice to create a match
-	if(openCards.length > 0 && cardClicked === openCards[0]){
-		console.log("Clicked the same card twice");
-		return;
-	}
+  // Don't do anything if the card has already been matched
+  if(cardClicked.classList.contains('match')) { return; }
+  // Prevent clicking the same card twice to create a match
+  if(openCards.length > 0 && cardClicked === openCards[0]){
+    console.log("Clicked the same card twice");
+    return;
+  }
 
-	// Start a new pair by resetting the revealed card timeout
-	if(timeoutId != null){
-		// Do not
-		if(cardClicked == openCards[0] || cardClicked == openCards[1]){
-			return;
-		}
-		if(openCards.length > 0){
-			clearTimeout(timeoutId);
-			rejectNonMatchingCards();
-		}
-	}
+  // Start a new pair by resetting the revealed card timeout
+  if(timeoutId != null){
+    // Do nothing if you clicked the open cards
+    if(cardClicked == openCards[0] || cardClicked == openCards[1]){
+      return;
+    }
+    if(openCards.length > 0){
+      clearTimeout(timeoutId);
+      rejectNonMatchingCards();
+    }
+  }
 
-	// Reveal card
-	toggleShowCard(cardClicked);
-	// Add the card to the openCards list
-	addToOpenList(cardClicked);
-	// Check openCards for a match
-	if(openCards.length > 1) {
-		checkForMatch();
-		incrementNumberOfMoves();
-	}
+  // Reveal card
+  toggleShowCard(cardClicked);
+  // Add the card to the openCards list
+  addToOpenList(cardClicked);
+  // Check openCards for a match
+  if(openCards.length > 1) {
+    checkForMatch();
+    incrementNumberOfMoves();
+  }
 
-	// Check if all pairs are complete
-	checkForWin();
+  // Check if all pairs are complete
+  checkForWin();
 });
 
 restartButton.addEventListener('click', function(event){
-	if(matchingPairsCount == 8){
-		resetGameElementsAfterWin();
-	}
-	stopTimer();
-	gameInit();
+  if(matchingPairsCount == 8){
+    resetGameElementsAfterWin();
+  }
+  stopTimer();
+  gameInit();
 });
 
 winPanel.addEventListener('click', function(event){
-	resetGameElementsAfterWin();
-	gameInit();
-})
+  resetGameElementsAfterWin();
+  gameInit();
+});
 
 container.addEventListener('animationend', function(event){
 	setTimeout(function(){container.classList.remove('matchPair');}, 1000);
@@ -88,231 +93,231 @@ container.addEventListener('animationend', function(event){
 
 // Card functions
 function toggleShowCard(card){
-	card.classList.toggle('show');
-	card.classList.toggle('open');
+  card.classList.toggle('show');
+  card.classList.toggle('open');
 }
 
 function checkForMatch(){
-	let cardOneClass = openCards[0].firstChild.classList[0];
-	let cardTwoClass = openCards[1].firstChild.classList[0];
+  let cardOneClass = openCards[0].firstChild.classList[0];
+  let cardTwoClass = openCards[1].firstChild.classList[0];
 
-	if(cardOneClass == cardTwoClass){
-		setMatchingCards();
-		matchingPairsCount++;
-	} else {
-		timeoutId = setTimeout(rejectNonMatchingCards, 1000);
-	}
+  if(cardOneClass == cardTwoClass){
+    setMatchingCards();
+    matchingPairsCount++;
+  } else {
+    timeoutId = setTimeout(rejectNonMatchingCards, 1000);
+  }
 }
 
 function checkForWin(){
-	if(matchingPairsCount === 8){
-		// Win condition met
-		stopTimer();
-		updateWinPanel();
-		showWinPanel();
-	}
+  if(matchingPairsCount === 8){
+    // Win condition met
+    stopTimer();
+    updateWinPanel();
+    showWinPanel();
+  }
 }
 
 function setMatchingCards(){
-	for(let i = 0; i < openCards.length; i++){
-		openCards[i].classList.add('match');
-	}
-	container.classList.add('matchPair');
+  for(let i = 0; i < openCards.length; i++){
+    openCards[i].classList.add('match');
+  }
+  container.classList.add('matchPair');
 
-	clearOpenCards();
+  clearOpenCards();
 }
 
 function rejectNonMatchingCards() {
-	for(let i = 0; i < openCards.length; i++){
-		toggleShowCard(openCards[i]);
-	}
+  for(let i = 0; i < openCards.length; i++){
+    toggleShowCard(openCards[i]);
+  }
 
-	clearOpenCards();
-	timeoutId = null;
+  clearOpenCards();
+  timeoutId = null;
 }
 
 function clearOpenCards(){
-	openCards.splice(0, 2);
+  openCards.splice(0, 2);
 }
 
 function addToOpenList(card){
-	openCards.push(card);
+  openCards.push(card);
 }
 
 function createArrayOfCards(){
-	cards = [];
-	// Hardcoded 8 types of cards for a 16 card game
-	const cardTypes = ["card-mushroom", "card-10coins", "card-20coins", "card-mario",
-	"card-castle", "card-flower","card-star", "card-1up"];
+  cards = [];
+  // Hardcoded 8 types of cards for a 16 card game
+  const cardTypes = ["card-mushroom", "card-10coins", "card-20coins", "card-mario",
+  "card-castle", "card-flower","card-star", "card-1up"];
 
-	// Add two of each card to the array of cards
-	for(let i = 0; i < cardTypes.length; i++){
-		cards.push(createCard(cardTypes[i]));
-		cards.push(createCard(cardTypes[i]));
-	}
+  // Add two of each card to the array of cards
+  for(let i = 0; i < cardTypes.length; i++){
+    cards.push(createCard(cardTypes[i]));
+    cards.push(createCard(cardTypes[i]));
+  }
 }
 
 function createCard(cardTypesText){
-	var cardElement = document.createElement("li");
-	cardElement.classList.add('card');
+  var cardElement = document.createElement("li");
+  cardElement.classList.add('card');
 
-	var innerImage = document.createElement("img");
-	innerImage.classList.add(cardTypesText);
-	innerImage.src = 'img/' + cardTypesText + '.png'
-	cardElement.appendChild(innerImage);
+  var innerImage = document.createElement("img");
+  innerImage.classList.add(cardTypesText);
+  innerImage.src = 'img/' + cardTypesText + '.png';
+  cardElement.appendChild(innerImage);
 
-	return cardElement;
+  return cardElement;
 }
 
 // Win panel functions
 function createWinPanel(){
-	winPanelHeader.textContent = "You win!";
-	winPanel.appendChild(winPanelHeader);
-	winPanel.appendChild(winPanelText);
-	winPanel.classList.add('container');
+  winPanelHeader.textContent = "You win!";
+  winPanel.appendChild(winPanelHeader);
+  winPanel.appendChild(winPanelText);
+  winPanel.classList.add('container');
 }
 
 function updateWinPanel(){
-	winPanelText.textContent = `You beat the game in ${movesCounter} moves, earning ${starsCount} stars. Click HERE to play again`;
+  winPanelText.textContent = `You beat the game in ${movesCounter} moves, earning ${starsCount} stars. Click HERE to play again`;
 }
 
 function showWinPanel(){
-	const parent = deck.parentElement;
-	deck.remove();
-	parent.appendChild(winPanel);
+  const parent = deck.parentElement;
+  deck.remove();
+  parent.appendChild(winPanel);
 }
 
 function resetGameElementsAfterWin(){
-	container.appendChild(deck);
-	winPanel.remove();
+  container.appendChild(deck);
+  winPanel.remove();
 }
 
 // Timer functions
 
 function createTimerPanel(){
-	timerMins.textContent = minutes;
-	timerSecs.textContent = seconds + "0";
-	let timerSpacer = document.createElement('span');
-	timerSpacer.textContent = ':';
+  timerMins.textContent = minutes;
+  timerSecs.textContent = seconds + "0";
+  let timerSpacer = document.createElement('span');
+  timerSpacer.textContent = ':';
 
-	timerPanel.appendChild(timerMins);
-	timerPanel.appendChild(timerSpacer);
-	timerPanel.appendChild(timerSecs);
-	scorePanel.appendChild(timerPanel);
+  timerPanel.appendChild(timerMins);
+  timerPanel.appendChild(timerSpacer);
+  timerPanel.appendChild(timerSecs);
+  scorePanel.appendChild(timerPanel);
 }
 
 function startTimer(){
-	startTime = Date.now();
-	console.log("startTimer run");
-	// Refresh the time in just under a
-	timerIntervalId = setInterval(updateTimer, 500);
+  startTime = Date.now();
+  console.log("startTimer run");
+  // Refresh the time in just under a
+  timerIntervalId = setInterval(updateTimer, 500);
 }
 
 function stopTimer(){
-	clearInterval(timerIntervalId);
-	updateTimer();
+  clearInterval(timerIntervalId);
+  updateTimer();
 }
 
 function updateTimer(){
-	console.log("UpdateTimer");
-	nowTime = Date.now();
+  console.log("UpdateTimer");
+  nowTime = Date.now();
 
-	let timeElapsed = nowTime - startTime;
-	let totalSeconds = Math.floor(timeElapsed/1000);
-	seconds = totalSeconds % 60;
-	minutes = (totalSeconds > 60) ? Math.floor(totalSeconds/60) : 0;
+  let timeElapsed = nowTime - startTime;
+  let totalSeconds = Math.floor(timeElapsed/1000);
+  seconds = totalSeconds % 60;
+  minutes = (totalSeconds > 60) ? Math.floor(totalSeconds/60) : 0;
 
-	// Push new time to page
-	timerSecs.textContent = (seconds < 10) ? "0" + seconds : seconds;
-	timerMins.textContent = minutes;
+  // Push new time to page
+  timerSecs.textContent = (seconds < 10) ? "0" + seconds : seconds;
+  timerMins.textContent = minutes;
 }
 
 function clearTimer(){
-	timerMins.textContent = "0";
-	timerSecs.textContent = "00";
+  timerMins.textContent = "0";
+  timerSecs.textContent = "00";
 }
 
 // Game setup functions
 function gameInit(){
-	// Create new array of cards
-	createArrayOfCards();
-	// Shuffle them using the Fisher-Yates/Knuth shuffle
-	shuffle(cards);
-	deck.innerHTML = '';
-	// Fill the deck with the newly shuffled cards
-	for(let i = 0; i < cards.length; i++){
-	 deck.appendChild(cards[i]);
-	}
+  // Create new array of cards
+  createArrayOfCards();
+  // Shuffle them using the Fisher-Yates/Knuth shuffle
+  shuffle(cards);
+  deck.innerHTML = '';
+  // Fill the deck with the newly shuffled cards
+  for(let i = 0; i < cards.length; i++){
+    deck.appendChild(cards[i]);
+  }
 
-	// Initialize move counter to zero
-	resetNumberOfMoves();
+  // Initialize move counter to zero
+  resetNumberOfMoves();
 
-	// Reset number of matching pairs
-	matchingPairsCount = 0;
+  // Reset number of matching pairs
+  matchingPairsCount = 0;
 
-	//Reset number of stars to 3
-	starsCount = 3;
-	resetStarsDisplay();
+  //Reset number of stars to 3
+  starsCount = 3;
+  resetStarsDisplay();
 
-	// Reset timer variables
-	startTime = minutes = seconds = nowTime = 0;
-	timerIntervalId = null;
-	clearTimer();
+  // Reset timer variables
+  startTime = minutes = seconds = nowTime = 0;
+  timerIntervalId = null;
+  clearTimer();
 }
 
 // Simple variable and DOM element reset
 function resetNumberOfMoves(){
-	movesCounter = 0;
-	numMovesSpan.textContent = movesCounter;
+  movesCounter = 0;
+  numMovesSpan.textContent = movesCounter;
 }
 
 // Reset the classList on all 3 stars
 function resetStarsDisplay(){
-	for(let i = 0; i < starsElements.length; i++){
-		starsElements[i].classList.add('fa-star');
-		starsElements[i].classList.remove('fa-star-o');
-	}
+  for(let i = 0; i < starsElements.length; i++){
+    starsElements[i].classList.add('fa-star');
+    starsElements[i].classList.remove('fa-star-o');
+  }
 }
 
 // Increment movesCounter and check for star rating change
 function incrementNumberOfMoves(){
-	 movesCounter++;
-	 numMovesSpan.textContent = movesCounter;
-	 if(movesCounter === 16){
-		 starsCount = 2;
-		 toggleStarsOff(2);
-	 }
+   movesCounter++;
+   numMovesSpan.textContent = movesCounter;
+   if(movesCounter === 16){
+     starsCount = 2;
+     toggleStarsOff(2);
+   }
 
-	 if(movesCounter === 20){
-		 starsCount = 1;
-		 toggleStarsOff(1);
-	 }
+   if(movesCounter === 20){
+     starsCount = 1;
+     toggleStarsOff(1);
+   }
 
-	 if(movesCounter === 24){
-		 starsCount = 0;
-		 toggleStarsOff(0);
-	 }
+   if(movesCounter === 24){
+     starsCount = 0;
+     toggleStarsOff(0);
+   }
 
  }
 
  function toggleStarsOff(index){
-	 starsElements[index].classList.remove('fa-star');
-	 starsElements[index].classList.add('fa-star-o');
+   starsElements[index].classList.remove('fa-star');
+   starsElements[index].classList.add('fa-star-o');
  }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-		var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
-		while (currentIndex !== 0) {
-				randomIndex = Math.floor(Math.random() * currentIndex);
-				currentIndex -= 1;
-				temporaryValue = array[currentIndex];
-				array[currentIndex] = array[randomIndex];
-				array[randomIndex] = temporaryValue;
-		}
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-		return array;
+  return array;
 }
 
 // Launch game on page load
